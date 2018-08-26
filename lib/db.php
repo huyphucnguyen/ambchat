d6fd4845ce7404409a2e7b9afcc9cf09dbc36d0a
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-08-26 11:47:58 +0700

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,3 +1,9 @@
 <?php
-echo('Not implement yet!');
+function getDatabase(){
+  $dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com 
+  port=5432 dbname=d4ieg9ce7qihnf user=doirzncaoefasd password=0955cadb61b87148265f253f9b11a740c24b806bbb7d9b24c2b992da74861a99")
+  or die("Connect falsed");
+  
+  return $dbconn;
+}
 ?>
