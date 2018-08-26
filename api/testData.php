ca2f83534eec243430b3ffa7ed146b2b28693e6b
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-08-26 16:49:55 +0700

    Update testData.php

--- api/testData.php
+++ api/testData.php
@@ -2,6 +2,6 @@
 include ('../lib/db.php');
     $dbconn = getDatabase();
     
-    $sql_dk = "insert into user value ('111','shokichi','123456','Sho','sho.png','shokichi@ovi.com',CURRENT_DATE";
+    $sql_dk = "insert into user values ('111','shokichi','123456','Sho','sho.png','shokichi@ovi.com',CURRENT_DATE";
     pg_query($dbconn,$sql_dk);
 ?>
