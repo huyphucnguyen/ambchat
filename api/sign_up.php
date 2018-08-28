96d99a6b2cddf71333786d10f8e81c6fa7946ea8
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-08-28 16:58:35 +0700

    Update sign_up.php

--- api/sign_up.php
+++ api/sign_up.php
@@ -15,20 +15,22 @@ if(isset($_POST['userName'])){
     $sql_user = "select * from user where user_name = {'$username'}";
     $result = pg_query($dbconn,$sql_dk);
     if(pg_num_rows($sql_dk)>0){
-        echo('{"value": -1, "message": "Tài khoản đã tồn tại!"}'); //Tài khoản đã tồn tại
+        echo('{"value": "-1"}'); //Tài khoản đã tồn tại
         exit();
     }
 
     $sql_email = "select * from user where email = {'$email'}";
     $rs_email = pg_query($dbconn,$sql_email);
     if(pg_num_rows($rs_email)>0){
-        echo('{"value": -2, "message": "Email đã được đăng ký!"}'); //Email đã được đăng ký bởi tài khoản khác
+        echo('{"value": "-2"}'); //Email đã được đăng ký bởi tài khoản khác
         exit();
     }
 
     $sql_dk = "insert into public.user(user_name,pass_word,full_name,picture,email,date_create) values ({'$username'},{'$password'},{'$fullName'},{'$image'},{'$email'},CURRENT_DATE)";
-    pg_query($dbconn,$sql_dk);
+    if(pg_query($dbconn,$sql_dk)){
+        echo('{"value": "1"}');
+    }
 
-    echo('{"value": 1, "message": "Đăng ký thành công!"}');
+    echo('{"value": "1"}');
 }
 ?>
