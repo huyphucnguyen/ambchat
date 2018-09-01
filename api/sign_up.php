1cb8a3a8bd306e730eb37dc7ecfe51eeb6c7e389
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-09-01 08:11:35 +0700

    Update sign_up.php

--- api/sign_up.php
+++ api/sign_up.php
@@ -23,7 +23,7 @@ if(isset($_POST['username'])&&isset($_POST['fullname'])&&isset($_POST['password'
   if($result!=null){
     if(pg_num_rows($sql_dk)==0){
       /*Kiểm tra email có người đăng ký chưa. Email của user là duy nhất*/
-      $sql_email = "select * from user where email = '$email'";
+      $sql_email = "select * from \"public\".\"user\" where email = '$email'";
       $rs_email = $dbconnection->select($sql_email);
       if(pg_num_rows($rs_email)==0){
         $sql_dk = "insert into \"public\".\"user(user_name,pass_word,full_name,picture,email,date_create) 
