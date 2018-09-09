8f5f28a6c6f9af67f85333ce92b75da141844e95
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-09-09 16:43:48 +0700

    Update sign_in.php

--- api/sign_in.php
+++ api/sign_in.php
@@ -7,8 +7,7 @@ $res = null;
 if (isset($_POST["username"]) && isset($_POST["password"])) {
 
     $username = $_POST["username"];
-    $password = $_POST["password"];
-
+    $password = hash('sha256',$_POST["password"]);
     //$querry1="select * from user where user_name='$username' ";
     $sql = "SELECT * FROM \"public\".\"user\" WHERE user_name='$username'";
 
