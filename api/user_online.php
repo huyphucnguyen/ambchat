4ce6f5a8e77173fe05a4dee21cac496eccd1b703
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-09-16 20:56:01 +0700

    Update user_online.php

--- api/user_online.php
+++ api/user_online.php
@@ -1,48 +1,47 @@
 <?php
-session_start();
 header('Content-Type: application/json');
-include "db.php";
+include "../lib/data.php";
+include "../lib/db.php";
 $res = null;
 
-$session = session_id();
-$time = time();
-$time_check = $time - 300; //set time online is 5 minutes
-
+//Connect database
 $dbconnection = new postgresql("");
-$sql = "select * from public.user_online where session = '$session'";
-$result = $dbconnection->select($sql);
 
-if($result!=null){
-    if(pg_num_rows($result) == 0){
-        $sql1 = "insert into public.user_online(session,time) values('$session','$time')";
-        $result1 = $dbconnection->select($sql1);
-        if($result==null)
-            $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-        $dbconnection->closeResult($result1);
+//Delete offline user
+$time = time();
+$time_check = $time - 300; //set online time is 5 minutes
+$sql_deleOff = "delete from public.user_online where time < $time_check";
+$dbconnection->execute($sql);
+
+//online user request
+if(isset($_GET['session'])&&isset($_GET['user_id'])&&isset($_GET['device_id'])){
+    $session = $_GET['session'];
+    $user_id = $_GET['user_id'];
+    $device_id = $_GET['device_id'];
+
+
+    $sq1 = "select * from public.user_online where user_id = '$user_id' and device_id = '$device_id'";
+
+
+    $result = $dbconnection->select($sql);
+
+    if($result!=null){
+        if(pg_num_rows($result) != 0){
+            //Nếu tồn tại thì update session
+            $sql_updateSS = "update public.user_online set session = '$session',time = '$time' where user_id = '$user_id' and device_id = '$device_id' ";
+            $dbconnection->execute($sql_updateSS);
+        }else{
+            //Không tồn tại thì insert mới
+            $sql_insertSS = "insert into public.user_online values('$session','$time','$user_id','$device_id') ";
+            $dbconnection->execute($sql_insertSS);
+        }
+        $dbconnection->closeResult($result);
+    }else{
+        $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
     }
-    else{
-        $sql2 = "update public.user_online set time='$time' where session = '$session'";
-        $result2 = $dbconnection->select($sql2);
-        if($result==null)
-            $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-        $dbconnection->closeResult($result2);
-    }
-
-    $sql3 = "select * from public.user_online";
-    $result3 = $dbconnection->select($sql3);
-    $count_user_online = pg_num_rows($result3);
-    $dbconnection->closeResult($result3);
-    $res = new Result(Constant::SUCCESS,'User online number: '.$count_user_online);
-
-    //Delete record when user is offline
-    $sql4 = "delete from public.user_online where time < $time_check";
-    $result4 = $dbconnection->select($sql4);
-    $dbconnection->closeResult($result4);
-    $dbconnection->closeResult($result);
     $dbconnection->close();
+}else{
+    $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
 }
-else
-    $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-echo $res;
 
-?>
+echo (json_encode($res));
