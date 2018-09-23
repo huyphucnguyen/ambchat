36621700788775fad2ed7a6fd41978aaaf54b8b3
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-09-23 16:21:16 +0700

    Create message.php

---
+++ api/message.php
@@ -0,0 +1,21 @@
+<?php 
+include "/lib/function.php";
+$result = null;
+$res = null;
+
+if(isset($_POST['session']) && isset($_POST['message'])){
+    $session = $_POST['session'];
+    $message = $_POST['message'];
+
+    $resutl = sendMessageToFCM($session,$message);
+    if($result!=null){
+        $res = new Result(Constant::SUCCESS,$message);
+    }
+    else{
+        $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+    }
+}
+else{
+    $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
+}
+echo $res;
