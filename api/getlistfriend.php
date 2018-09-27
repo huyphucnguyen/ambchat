c889182fb69a0e57fdc03aae24959ebca5af5142
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-09-27 13:27:36 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -4,24 +4,21 @@ include "../lib/data.php";
 $dbconnection = new postgresql("");
 $sql = "SELECT * FROM \"public\".\"user\" ";
 $data = $dbconnection->select($sql);
-//$data = pg_query($dbconn,$sql);
- 
- /*class User{
+class User{
       function User($user_id,$user_name,$fullname){
             $this->User_ID=$id;
             $this->User_Name=$user_name;
             $this->FullName=$fullname;
       }
- }*/
+ }
    // tao mang
-   //   $mangUser=array();
+     $mangUser=array();
    //Them phan tu vao amng
      while ($row=pg_fetch_array($data)) 
      {
-       // array_push($mangUser, new User( $row['user_id'],$row['user_name'],$row['full_name']));
-      echo $row['user_name'];
-     
+        array_push($mangUser, new User( $row['user_id'],$row['user_name'],$row['full_name']));
+        echo $row['user_name'];  
      }      
     // Chuyen dinh dang cua mang thanh JSON
-     //echo count(mangUser);
+     echo json_encode(mangUser);
 ?>
