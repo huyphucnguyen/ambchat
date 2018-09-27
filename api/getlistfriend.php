273b3cf326329fa601c670bac963b930c34fe336
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-09-27 13:20:37 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -7,20 +7,21 @@ $dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com
     
     $data = pg_query($dbconn,$sql);
  
- class User{
+ /*class User{
       function User($user_id,$user_name,$fullname){
             $this->User_ID=$id;
             $this->User_Name=$user_name;
             $this->FullName=$fullname;
       }
- }
+ }*/
    // tao mang
-      $mangUser=array();
+   //   $mangUser=array();
    //Them phan tu vao amng
-     while ($row=pg_fetch_assoc($data)) 
+     while ($row=pg_fetch_array($data)) 
      {
-        array_push($mangUser, new User( $row['user_id'],$row['user_name'],$row['full_name']));
+       // array_push($mangUser, new User( $row['user_id'],$row['user_name'],$row['full_name']));
+      echo $row['user_name'];
      }      
     // Chuyen dinh dang cua mang thanh JSON
-      echo count(mangUser);
+     //echo count(mangUser);
 ?>
