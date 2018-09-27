7b2deab33985026ef4d5a2ccbc0fb783723f6be8
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-09-27 13:42:18 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -6,12 +6,12 @@ $sql = "SELECT * FROM \"public\".\"user\" ";
 $data = $dbconnection->select($sql);
 
    // tao mang
-     $arrUser=array();
+   
    //Them phan tu vao amng
      while ($row=pg_fetch_array($data)) 
      {
         //array_push($arrUser, new User("aaaaa"));
-        echo "So phan tu cua mang la ". $row['user_id'];
+        echo "So phan tu cua mang la ";
        // echo "So phan tu cua mang la ";  
      }      
     // Chuyen dinh dang cua mang thanh JSON
