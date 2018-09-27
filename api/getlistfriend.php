353bf2cea8b51e104097aada4c40d7a629d6d9a8
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-09-27 13:25:53 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -1,11 +1,10 @@
 <?php 
-$dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com
-  port=5432 dbname=d4ieg9ce7qihnf user=doirzncaoefasd password=0955cadb61b87148265f253f9b11a740c24b806bbb7d9b24c2b992da74861a99");
- 
-  $sql = "SELECT * FROM \"public\".\"user\" ";
-     //$dbconnection=getDatabase();
-    
-    $data = pg_query($dbconn,$sql);
+header('Content-Type: application/json');
+include "../lib/data.php";
+$dbconnection = new postgresql("");
+$sql = "SELECT * FROM \"public\".\"user\" ";
+$data = $dbconnection->select($sql);
+//$data = pg_query($dbconn,$sql);
  
  /*class User{
       function User($user_id,$user_name,$fullname){
@@ -21,6 +20,7 @@ $dbconn = pg_connect("host=ec2-54-227-241-179.compute-1.amazonaws.com
      {
        // array_push($mangUser, new User( $row['user_id'],$row['user_name'],$row['full_name']));
       echo $row['user_name'];
+     
      }      
     // Chuyen dinh dang cua mang thanh JSON
      //echo count(mangUser);
