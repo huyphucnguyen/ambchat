68669d8cd9bcd52b3f5196ef82fc60fa856a79ba
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-10-19 14:48:19 +0700

    Update firebase.php

--- lib/firebase.php
+++ lib/firebase.php
@@ -11,35 +11,35 @@ class Firebase{
 
 /**function makes curl request to firebase servers
 *@param $fields is data with message and destination*/
-  private function sendPushNotification($fields){
-    define('FIREBASE_API_KEY','AAAAcEvRAgI:APA91bEYss_1nNoJjUWaOILYPuKUeyrcfCjXOf2gT4Z_Bi_k71CzMSNZNOwcNnabxLfqn71dN4lcBL4gFgIwYOvvXuWCFUFV5tBxnyWYj1XCEJoDOYqWhOHyvq3xdSR4CwglxM1mAORR');
+//   private function sendPushNotification($fields){
+//     define('FIREBASE_API_KEY','AAAAcEvRAgI:APA91bEYss_1nNoJjUWaOILYPuKUeyrcfCjXOf2gT4Z_Bi_k71CzMSNZNOwcNnabxLfqn71dN4lcBL4gFgIwYOvvXuWCFUFV5tBxnyWYj1XCEJoDOYqWhOHyvq3xdSR4CwglxM1mAORR');
     
-    $url = 'https://fcm.googleapis.com/fcm/send';
-    $headers = array(
-      'Authorization: key=' .FIREBASE_API_KEY,
-      'Content-Type: application/json'
-    );
-    //Opent connection
-    $ch = curl_init();
+//     $url = 'https://fcm.googleapis.com/fcm/send';
+//     $headers = array(
+//       'Authorization: key=' .FIREBASE_API_KEY,
+//       'Content-Type: application/json'
+//     );
+//     //Opent connection
+//     $ch = curl_init();
     
-    //Set the url, number POST vars, POST data
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_POST,true);
-    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+//     //Set the url, number POST vars, POST data
+//     curl_setopt($ch,CURLOPT_URL,$url);
+//     curl_setopt($ch,CURLOPT_POST,true);
+//     curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+//     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
     
-    //Disabling SSL Certificate support temporarly
-    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($fields));
+//     //Disabling SSL Certificate support temporarly
+//     curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+//     curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($fields));
     
-    //Execute
-    $result = curl_exec($ch);
-    if($result === FALSE){
-      die('Curl failed: '.curl_error($ch));
-    }
+//     //Execute
+//     $result = curl_exec($ch);
+//     if($result === FALSE){
+//       die('Curl failed: '.curl_error($ch));
+//     }
     
-    //close connection
-    curl_close();
+//     //close connection
+//     curl_close();
     
-    return $result; // TRUE
+//     return $result; // TRUE
   }
