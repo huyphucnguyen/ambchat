0c93d4749feb6ab6c8535fdf81801e07a8cffdb9
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-10-19 14:53:29 +0700

    Update message.php

--- api/message.php
+++ api/message.php
@@ -7,8 +7,6 @@ if(isset($_GET['device_id']) && isset($_GET['message'])){
     $device_id = $_GET['device_id'];
     $message = $_GET['message'];
     
-    $res = new Result(Constant::GENERAL_ERROR, 'Can not send the message. Please try again later.');
-    
     include "../lib/PushMessage.php";
     include "../lib/firebase.php";
     
