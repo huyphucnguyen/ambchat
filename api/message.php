1bb7f68705c205f674db7d35539f062f2dad24e1
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-10-19 14:38:11 +0700

    Update message.php

--- api/message.php
+++ api/message.php
@@ -7,26 +7,26 @@ if(isset($_GET['device_id']) && isset($_GET['message'])){
     $device_id = $_GET['device_id'];
     $message = $_GET['message'];
     
-    include "../lib/PushMessage.php";
-    include "../lib/firebase.php";
+//     include "../lib/PushMessage.php";
+//     include "../lib/firebase.php";
     
-    $firebase = new Firebase();
-    $push = new PushMessage();
+//     $firebase = new Firebase();
+//     $push = new PushMessage();
     
-    $push->setMessage($message);
-    $json = '';
-    $response = '';
+//     $push->setMessage($message);
+//     $json = '';
+//     $response = '';
     
-    $json = $push -> getPush();
-    $response = $firebase->send($device_id,$message);
+//     $json = $push -> getPush();
+//     $response = $firebase->send($device_id,$message);
     
     
-    if($response==TRUE){
-        $res = new Result(Constant::SUCCESS,$json);
-    }
-    else{
-        $res = new Result(Constant::GENERAL_ERROR, 'Can not send the message. Please try again later.');
-    }
+//     if($response==TRUE){
+//         $res = new Result(Constant::SUCCESS,$json);
+//     }
+//     else{
+//         $res = new Result(Constant::GENERAL_ERROR, 'Can not send the message. Please try again later.');
+//     }
 }
 else{
     $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
