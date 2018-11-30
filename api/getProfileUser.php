2b47b9fce2b7c4932b3e67af2e5afa7774ca074c
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2018-11-30 11:21:14 +0700

    Update getProfileUser.php

--- api/getProfileUser.php
+++ api/getProfileUser.php
@@ -10,11 +10,11 @@ if(isset($_POST["username"])){
      include "../lib/db.php";
      //$dbconnection=getDatabase();
      $dbconnection = new postgresql("");
-     $result = $dbconnection->select($sql);
-      if ($result !== null) {
-        if (pg_num_rows($result) > 0) {
-            //user exist
-       
+	 
+	 if($dbconnection->isValid()){
+	   $result = $dbconnection->select($sql);
+       if ($result !== null) {
+         if (pg_num_rows($result) > 0) {
             $user = null;
             while ($data = pg_fetch_object($result)) {
                 $user = $data;
@@ -23,17 +23,23 @@ if(isset($_POST["username"])){
             $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
                     unset($user->pass_word);
                     $res->data = $user;
-        } else {
+         } else {
             $res = new Result(Constant::INVALID_USER, 'User is not exist');
         }
         $dbconnection->closeResult($result);
     } else {
         $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-    }
-    $dbconnection->close();
+        }
+        $dbconnection->close();
+	}
+      else {
+			   $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+		 }
+    
 
 }   else {
-    $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
+              $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
 }
 echo (json_encode($res));
+
 ?>
