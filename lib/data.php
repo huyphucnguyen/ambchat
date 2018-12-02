1a05366c654895abfc97b76fd16fab8e3a7e7196
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2018-12-02 15:45:06 +0700

    Update data.php

--- lib/data.php
+++ lib/data.php
@@ -18,16 +18,16 @@ class Result
     }
 }
 
-class User{
-    private User_ID;
-    private User_Name;
-    private Full_Name;
-     function User($user_id,$user_name,$full_name){
-         $this->User_ID=$user_id;
-         $this->User_Name=$user_name;
-         $this->Full_Name=$full_name;
-    }
- }
+// class User{
+//     private User_ID;
+//     private User_Name;
+//     private Full_Name;
+//      function User($user_id,$user_name,$full_name){
+//          $this->User_ID=$user_id;
+//          $this->User_Name=$user_name;
+//          $this->Full_Name=$full_name;
+//     }
+//  }
 
 class Constant
 {
