79e29f7cb79645b9eda00de1aca2f25809742c80
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-01-02 18:14:44 +0700

    Update function.php
    Add function encrypt data

--- lib/function.php
+++ lib/function.php
@@ -20,6 +20,32 @@ function createJsonWebToken($data){
     return $jwt;
 }
 
+function encryptData($data,$key){
+    $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+    $iv = openssl_random_pseudo_bytes($ivlen);
+    $ciphertext_raw = openssl_encrypt($data, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+    $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+    $ciphertext = base64_encode( $iv.$hmac.$ciphertext_raw );
+    return $ciphertext;
+}
+
+function dencryptData($key){
+    $c = base64_decode($ciphertext);
+    $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+    $iv = substr($c, 0, $ivlen);
+    $hmac = substr($c, $ivlen, $sha2len=32);
+    $ciphertext_raw = substr($c, $ivlen+$sha2len);
+    $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+    $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+    if (hash_equals($hmac, $calcmac))//PHP 5.6+ timing attack safe comparison
+    {
+        return $original_plaintext;
+    }
+    return null;
+    ?>
+}
+
+
 
 function setOnline($dbconnection,$user_id,$device_id){
     //Tiến hành ghi bảng user_history và xóa bỏ các records có guid của bảng user_online
