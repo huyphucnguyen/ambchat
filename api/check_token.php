0441d8fa232992f3e4499d8d751c7fc7a167ce6a
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-01-02 20:13:13 +0700

    Update check_token.php

--- api/check_token.php
+++ api/check_token.php
@@ -4,68 +4,62 @@ include "../lib/data.php";
 $res = null;
 if (isset($_POST["TOKEN"])) {
     $token = $_POST["TOKEN"];
+    //Decrypt token is sent by client
+    $token_decrypt = dencryptData($token,KEY_ENCRYPT);
+    //decode json to class
+    $token_data = json_decode($token_decrypt);
+    $user_id = $token_data->user_id;
+    $time_start = $token_data->time_start;
+    $time_life  = $token_data->time_life;
     
-    $sql = "SELECT * FROM \"public\".\"user\" WHERE token='$token'";
-    
-    // ket noi database
-    include "../lib/db.php";
-    $dbconnection = new postgresql("");
-    if($dbconnection->isValid()){
-        $result = $dbconnection->select($sql);
-        if ($result !== null) {
-          if (pg_num_rows($result) > 0) {
-            //Ton tại thì cập nhât lại token và trả về cho client
-            $user_id = null;
-            $device_id = null;
-            while ($data = pg_fetch_object($result)) {
-              $user_id = $data->user_id;
-              $device_id = $data->device_id;
-              break;
-            }
-            
-            //Cập nhật token
-            $sql_info = "SELECT * FROM public.user WHERE user_id = '$user_id'";
-            $result_info = $dbconnection->select($sql_info);
-            if($result_info!==null){
-              if(pg_num_rows($result_info) > 0){
+    //Take time at the present time
+    $time_now = time();
+    if(($time_start+$time_life)>=$time_now){
+        //token is alive => return user data
+        $sql = "SELECT * FROM public.user WHERE user_id = '$user_id'";
+        
+        // ket noi database
+        include "../lib/db.php";
+        $dbconnection = new postgresql("");
+        if($dbconnection->isValid()){
+            $result = $dbconnection->select($sql);
+            if ($result !== null) {
+              if (pg_num_rows($result) > 0) {
                 $user = null;
                 while ($data = pg_fetch_object($result)) {
-                    $user = $data;
-                    break;
+                  $user = $data;
+                  break;
                 }
-                unset($user->pass_word);
+                unset($user->pass_word);             
                 $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
                 $res->data = $user;
                 $guid = GUID();
                 $res->sesson_key = $guid;
-                
-                
                 //Set online status that was login successfully
-                setOnline($dbconnection,$user_id,$device_id);
-                
-                  //Tạo token & save token vào db
-                  $token = createJsonWebToken($res);
-                  //Xóa token cũ (nếu có)
-                  $sql_delete_token = "DELETE FROM public.token WHERE user_id = '$user_id' and device_id = '$device_id'";
-                  $sql_insert_token = "INSERT INTO public.token VALUES('$user_id','$device_id','$token')";
-                  $dbconnection->execute($sql_delete_token);
-                  $dbconnection->execute($sql_insert_token);
+                //setOnline($dbconnection,$user_id,$device_id);
+                  
+                //Token: user_id, timestart, timelife
+                date_default_timezone_set("Asia/Ho_Chi_Minh");
+                $time = time();
+                $token_raw = (json_encode(new Token($user_id,$time,604800)));
+                //Encrypt token
+                $token = encryptData($token_raw,KEY_ENCRYPT);
+                //Thêm token
+                $res->token = $token;
               }else{
                 $res = new Result(Constant::INVALID_USER , 'User is invalid.');  
               }
             }else{
               $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
             }
-          }else{
-            $res = new Result(Constant::INVALID_TOKEN, 'Token is not exist');
-          }
         }else{
-          $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+           $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
         }
     }else{
-       $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
+        //Token is die
+        $res = new Result(Constant::INVALID_TOKEN , 'Token is invalid.');  
     }
 }else{
   $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
 }
-echo createJsonWebToken($res);
+echo json_encode($res);
