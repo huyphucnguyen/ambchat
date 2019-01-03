20334fed332da1f63bea36ff401052ca5aa02de2
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-01-03 16:22:31 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -3,44 +3,35 @@
     header('Content-Type: application/json');
     include "../lib/data.php"; 
     include "../lib/db.php";
-	$res = null;
-	$sql = "SELECT * FROM \"public\".\"user\" ";
+    $res = null;
+    $sql = "SELECT * FROM \"public\".\"user\" ";
     $dbconnection = new postgresql("");
-	
-	if($dbconnection->isValid())
-	  {
-		  
-		   $result = $dbconnection->select($sql);
-		   ////
-		   
-		  if($result!==null){
-			   
-			     // create array user
+
+    if($dbconnection->isValid()) {
+   	$result = $dbconnection->select($sql);
+	if($result!==null){
+		// create array user
                 $arrUser=array();
-				$data=null;
+		$data=null;
                  //add element to arrUser
                 while ($row=pg_fetch_array($result)) 
                 {
-                      array_push($arrUser, 
-					             new User(
-								 $row['user_id'],
-								 $row['user_name'],
-								 $row['full_name']));
+                      array_push($arrUser, new User(
+						 $row->user_id,
+						 $row->user_name,
+					     	 $row->full_name));
                 }
 			  
-			  $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');     
-              $res->data = $arrUser;	
-		  }
-		  else{
-			    $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-			  }
-			  
-	     $dbconnection->close();
+		$res = new Result(Constant::SUCCESS, 'Operation complete successfully.');     
+                $res->data = $arrUser;	
+	}  else{
+		$res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+	}
+	$dbconnection->close();
          	 
-	  }
-	  else{
+	} else{
 	        $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-	  }
+	}
     // Chuyen dinh dang cua mang thanh JSON
     echo (json_encode($res));
 ?>
