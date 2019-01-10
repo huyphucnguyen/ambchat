d4f383327aedf48e29b8eed3c14360f37e1e6e18
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-01-10 21:27:17 +0700

    Update function.php
    Add function getStatusUser($dbconnection,$user_id)

--- lib/function.php
+++ lib/function.php
@@ -9,17 +9,6 @@ function GUID()
     return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
 }
 
-function createJsonWebToken($data){
-    $header = json_encode(['type' => 'JWT','alg' => 'SH256']);   
-    $payload = json_encode($data);
-    $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
-    $base64UrlPayload =  str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
-    $signature = hash_hmac('sha256',$base64UrlHeader . "." . $base64UrlPayload, 'Ambchat', true);
-    $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-    $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
-    return $jwt;
-}
-
 function encryptData($data,$key){
     $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
     $iv = openssl_random_pseudo_bytes($ivlen);
@@ -69,33 +58,29 @@ function setOnline($dbconnection,$user_id,$device_id){
     }    
 }
 
-function sendMessageToFCM($session = "" , $message = ""){
-    $url = "https://fcm.googleapis.com/fcm/send";
-    $serverKey = "AIzaSyBdWHaZD_B4MqS7Ja6BEP_TQthafMZO8t8";
-
-    $fields = array();
-    $fields['data'] = $message;
-    if (is_array($session)) {
-        $fields['registration_ids'] = $session;
-    } else {
-        $fields['to'] = $session;
+//function get status online or ofline
+function getStatusUser($dbconnection,$user_id){
+    $sql = "SELECT public.user_online WHERE user_id = '$user_id'";
+    $result = $dbconnection->select($sql);
+    if($sql!==null){
+        if(pg_num_rows($result) > 0){
+            date_default_timezone_set("Asia/Ho_Chi_Minh"); 
+            $time_now = time();
+            while ($data = pg_fetch_object($result)) {
+                $time_start = $data->time_start;
+                $time_life = $data->time_life;
+                
+                if($time_start+$time_life >= $time_now){
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        else{
+            return 0;
+        }
     }
-    $headers = array(
-        'Content-Type:application/json',
-        'Authorization:key=' . $serverKey
-    );
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
-    $result = curl_exec($curl);
-    if ($result === FALSE) {
-        die('FCM Send Error: '  .curl_error($curl));
+    else{
+        return 0;
     }
-    curl_close($curl);
-    return $result;
 }
