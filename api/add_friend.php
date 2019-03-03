a5c96fe41a985b57d71c7f4e4351fc45de6213a2
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-03 13:22:53 +0700

    Update add_friend.php

--- api/add_friend.php
+++ api/add_friend.php
@@ -27,10 +27,11 @@ if(isset($_GET['user_id'])&&isset($_GET['friend_id'])){
       else{
         $data = pg_fetch_object($result);
         $str_frients = $data->friend_id_list;
-        echo "str = '$str_frients'";
+        
         if($str_friends.strlen()!=0){
           $str_friends.= ',';
         }
+       echo "str = '$str_frients'";
         $str_friends=$str_friends.$friend_id;
        echo $str_friends;
        $sql_update = "UPDATE public.friends SET friend_id_list = '$str_friends' WHERE user_id = '$user_id'";
