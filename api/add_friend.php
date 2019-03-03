d9a0cc9845f4a0badbac7fb832732b54af8b4f15
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-03 13:35:48 +0700

    Update add_friend.php

--- api/add_friend.php
+++ api/add_friend.php
@@ -26,11 +26,11 @@ if(isset($_GET['user_id'])&&isset($_GET['friend_id'])){
       //TH2: User is not exits
       else{
         $data = pg_fetch_object($result);
-        $str_frients = $data->friend_id_list;
-        $arr = explode(',',$str_frients,-1);
-//         if($str_friends.strlen()!=0){
-//           $str_friends.= ',';
-//         }
+        $str_friends = $data->friend_id_list;
+      
+        if($str_friends.strlen()!=0){
+          $str_friends.= ',';
+        }
        $str_friends = implode(',',$arr);
         $str_friends=$str_friends.$friend_id;
        echo $str_friends;
