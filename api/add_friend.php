f63c76014f720b7237ee3751c2891cae84eb00ff
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-03 12:44:39 +0700

    Update add_friend.php

--- api/add_friend.php
+++ api/add_friend.php
@@ -23,16 +23,16 @@ if(isset($_GET['user_id'])&&isset($_GET['friend_id'])){
       } //pg_num_rows($result)>1
       //TH2: User is not exits
       else{
-        $sql1 = "SELECT friend_id_list FROM public.friends WHERE user_id = '$user_id'";
-        $result1 = $dbconnection->select($sql1);
-        $str_frients = (pg_fetch_object($result1))->friend_id_list;
-        if($str_friends!=null){
-          $str_friends.= ',';
-        }
-        $str_friends.="{$friend_id}";
+//         $sql1 = "SELECT friend_id_list FROM public.friends WHERE user_id = '$user_id'";
+//         $result1 = $dbconnection->select($sql1);
+//         $str_frients = (pg_fetch_object($result1))->friend_id_list;
+//         if($str_friends!=null){
+//           $str_friends.= ',';
+//         }
+//         $str_friends.="{$friend_id}";
        
-       $sql_update = "UPDATE public.friends SET friend_id_list = '$str_friends' WHERE user_id = '$user_id'";
-       $dbconnection->execute($sql_update);
+//        $sql_update = "UPDATE public.friends SET friend_id_list = '$str_friends' WHERE user_id = '$user_id'";
+//        $dbconnection->execute($sql_update);
         $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
       }
     
