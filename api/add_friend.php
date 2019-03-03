cc8b695691ef7e47faf2204ddce739b7bbfdfcb3
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-03 12:51:51 +0700

    Update add_friend.php

--- api/add_friend.php
+++ api/add_friend.php
@@ -1,6 +1,7 @@
 <?php 
  header('Content-Type: application/json');
 $res = null;
+include "../lib/data.php";
 if(isset($_GET['user_id'])&&isset($_GET['friend_id'])){
   $user_id = $_GET['user_id'];
   $friend_id = $_GET['friend_id'];
@@ -10,29 +11,29 @@ if(isset($_GET['user_id'])&&isset($_GET['friend_id'])){
   include '../lib/db.php';
   $dbconnection = new postgresql("");
   if($dbconnection->isValid()){
-//     $sql = "SELECT user_id FROM public.friends where user_id = '$user_id'";
-//     $result = $dbconnection->select($sql);
+     $sql = "SELECT user_id FROM public.friends where user_id = '$user_id'";
+     $result = $dbconnection->select($sql);
   
     if($result !==null){
       //TH1: User is exits
        if(pg_num_rows($result)<=0){
-//         //ex: "'1','444','0545'"
-//         $sql_i = "INSERT INTO public.friends VALUES('$user_id','$friend_id')";
-//         $dbconnection->execute($sql_i);
+        //ex: "'1','444','0545'"
+        $sql_i = "INSERT INTO public.friends VALUES('$user_id','$friend_id')";
+        $dbconnection->execute($sql_i);
          $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
       } //pg_num_rows($result)>1
       //TH2: User is not exits
       else{
-//         $sql1 = "SELECT friend_id_list FROM public.friends WHERE user_id = '$user_id'";
-//         $result1 = $dbconnection->select($sql1);
-//         $str_frients = (pg_fetch_object($result1))->friend_id_list;
-//         if($str_friends!=null){
-//           $str_friends.= ',';
-//         }
-//         $str_friends.="{$friend_id}";
+        $sql1 = "SELECT friend_id_list FROM public.friends WHERE user_id = '$user_id'";
+        $result1 = $dbconnection->select($sql1);
+        $str_frients = (pg_fetch_object($result1))->friend_id_list;
+        if($str_friends!=null){
+          $str_friends.= ',';
+        }
+        $str_friends.="{$friend_id}";
        
-//        $sql_update = "UPDATE public.friends SET friend_id_list = '$str_friends' WHERE user_id = '$user_id'";
-//        $dbconnection->execute($sql_update);
+       $sql_update = "UPDATE public.friends SET friend_id_list = '$str_friends' WHERE user_id = '$user_id'";
+       $dbconnection->execute($sql_update);
         $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
       }
     
