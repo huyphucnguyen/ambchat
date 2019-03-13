e8b27d57a6c8388c217306889f6379a84bdecef0
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-13 20:47:58 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -19,43 +19,43 @@ if(isset($_POST['user_id'])){
 	  $arr = explode(",",$friend_list);
           $size = sizeof($arr);
 	  $true_friends_list = array();
-	  for($i = 0;$i < $size ; $i++){
-		$fr_id = $arr[i];
-		  echo $fr_id.',';
-		//Kiem tra trong database có dòng user_id là $fr chứa $user_id không?
-		$sql2 = "SELECT * FROM public.friends WHERE user_id = '{$fr_id}'";
-		$result2 = $dbconnection->select($sql2);
-		if($result2 !== null){
-		   if(pg_num_rows($result2)>0){
-			$data = pg_fetch_object($result2);
-			$str_friends = $data->friend_id_list;
-			$arr2 = explode(",",$str_friends);
-			if(in_array($user_id,$arr2)){
-			    $sql3 = "SELECT user_id,full_name,picture,email,gender,
-			    user_id,phone FROM public.user WHERE user_id = '$fr_id'";
-			    $result3 = $dbconnection->select($sql2);
-			    if($result3!==null){
-				$data_fr = pg_fetch_object($result3);
-				array_push($true_friends_list,$data_fr);
-				$dbconnection->closeResult($result3);
-			    }//$result3!=null;
-			    else{
-				$res = new Result(Constant::GENERAL_ERROR,
-				   'There was an error while processing request. Please try again later.');
-			    }
+// 	  for($i = 0;$i < $size ; $i++){
+// 		$fr_id = $arr[i];
+// 		  echo $fr_id.',';
+// 		//Kiem tra trong database có dòng user_id là $fr chứa $user_id không?
+// 		$sql2 = "SELECT * FROM public.friends WHERE user_id = '{$fr_id}'";
+// 		$result2 = $dbconnection->select($sql2);
+// 		if($result2 !== null){
+// 		   if(pg_num_rows($result2)>0){
+// 			$data = pg_fetch_object($result2);
+// 			$str_friends = $data->friend_id_list;
+// 			$arr2 = explode(",",$str_friends);
+// 			if(in_array($user_id,$arr2)){
+// 			    $sql3 = "SELECT user_id,full_name,picture,email,gender,
+// 			    user_id,phone FROM public.user WHERE user_id = '$fr_id'";
+// 			    $result3 = $dbconnection->select($sql2);
+// 			    if($result3!==null){
+// 				$data_fr = pg_fetch_object($result3);
+// 				array_push($true_friends_list,$data_fr);
+// 				$dbconnection->closeResult($result3);
+// 			    }//$result3!=null;
+// 			    else{
+// 				$res = new Result(Constant::GENERAL_ERROR,
+// 				   'There was an error while processing request. Please try again later.');
+// 			    }
 					
-			}//in_array($user_id,$arr2)
-		    }//pg_num_rows($result2)>0
-		    else{
-			$res = new Result(Constant::INVALID_USER, 'User is not exist');
-		    }
-		    $dbconnection->closeResult($result2);
-		}//$result2 !== null
-		else{
-			$res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-		}
+// 			}//in_array($user_id,$arr2)
+// 		    }//pg_num_rows($result2)>0
+// 		    else{
+// 			$res = new Result(Constant::INVALID_USER, 'User is not exist');
+// 		    }
+// 		    $dbconnection->closeResult($result2);
+// 		}//$result2 !== null
+// 		else{
+// 			$res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+// 		}
 		
-	}//for()
+// 	}//for()
 	$res->data = $true_friends_list;
         $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
       } //pg_num_rows($result)>1
