09e9e1ef0b64ba35896d87de8a805d9791278ee1
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-13 20:45:12 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -9,7 +9,7 @@ if(isset($_POST['user_id'])){
   include '../lib/db.php';
   $dbconnection = new postgresql("");
   if($dbconnection->isValid()){
-     $sql = "SELECT * FROM public.friends WHERE user_id = '{$user_id}'";
+     //$sql = "SELECT * FROM public.friends WHERE user_id = '{$user_id}'";
      $result = $dbconnection->select($sql);
     if($result !==null){
 	$user = null;
