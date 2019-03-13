b96372e41f3f73eb49f365ac2f40707bd6f45f80
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-13 21:57:57 +0700

    Update getlistfriend.php

--- api/getlistfriend.php
+++ api/getlistfriend.php
@@ -32,7 +32,7 @@ if(isset($_POST['user_id'])){
 			if(in_array($user_id,$arr2)){
 			    $sql3 = "SELECT user_id,full_name,picture,email,gender,
 			    user_id,phone FROM public.user WHERE user_id = '$fr_id'";
-			    $result3 = $dbconnection->select($sql2);
+			    $result3 = $dbconnection->select($sql3);
 			    if($result3!==null){
 				$data_fr = pg_fetch_object($result3);
 				array_push($true_friends_list,$data_fr);
