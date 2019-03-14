b1f79dc635706135c559eb5917c60f320e4adc66
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-14 21:06:34 +0700

    Update getRequestListFriends.php

--- api/getRequestListFriends.php
+++ api/getRequestListFriends.php
@@ -8,31 +8,31 @@ if(isset($_POST['user_id'])){
     //init database
     include "../lib/db.php";
     $dbconnection = new postgresql("");
-//     if($dbconnection->isValid()){
-//         $result = dbconnection->select($sql);
-// //         if($result!==null){
-// //             if(pg_num_rows($result)>0){
-// //                 $data = pg_fetch_object($result);
-// //                 $request_id = $data->$request_id;
-// //                 $arr = explode(",",$request_id);
-// //                 $sql = "select * from public.user where user_id in '{$arr}'";
-// //                 $result1 = dbconnection->select($sql);
-// //                 $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
-// //                 $true_friends_list = pg_fetch_array($result1);
-// // 	              $res->data = $true_friends_list;
-// //             }//pg_num_rows($result)>0
-// //             else {
-// //                 $res = new Result(Constant::INVALID_USER, 'User is not exist');
-// //             }
-// //         }//$result!==null
-// //         else {
-// //             $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-// //         }
-//         $dbconnection->close();
-//     }//$dbconnection->isValid()
-//     else {
-//         $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
-//     }
+    if($dbconnection->isValid()){
+        $result = $dbconnection->select($sql);
+        if($result!==null){
+            if(pg_num_rows($result)>0){
+                $data = pg_fetch_object($result);
+                $request_id = $data->$request_id;
+                $arr = explode(",",$request_id);
+                $sql = "select * from public.user where user_id in '{$arr}'";
+                $result1 = $dbconnection->select($sql);
+                $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
+                $true_friends_list = pg_fetch_array($result1);
+	              $res->data = $true_friends_list;
+            }//pg_num_rows($result)>0
+            else {
+                $res = new Result(Constant::INVALID_USER, 'User is not exist');
+            }
+        }//$result!==null
+        else {
+            $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+        }
+        $dbconnection->close();
+    }//$dbconnection->isValid()
+    else {
+        $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
+    }
 }//isset($_POST['user_id'])
 else {
     $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
