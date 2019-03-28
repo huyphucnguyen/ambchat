b58949652c505d8642515cdacc0eabc011c452d2
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-03-28 21:04:41 +0700

    Update remove_friend.php

--- api/remove_friend.php
+++ api/remove_friend.php
@@ -15,25 +15,57 @@ if(isset($_POST['user_id'])&&isset($_POST['friend_id'])){
     if($result!==null){
       if(pg_num_rows($result)>0){
           //Xoa ban be
-              $arr = explode(",",$friend_id_list);
-              if (in_array($user_id, $arr)) 
-              {
-                  if(sizeof($arr)>1){
-                    unset($arr[array_search($friend_id,$arr)]);
-                    $string = implode(",",$arr);
-                    //update
-                    $sql = "UPDATE public.friends SET friend_id_list = '$string' WHERE user_id = '$user_id'";
-                    $dbconnection->execute($sql);
-                  } //sizeof($arr2)>1
-                  else {
-                    $sql = "DELETE FROM public.friends WHERE user_id = '$user_id'"; 
-                    $dbconnection->execute($sql);
-                  }  
-                  $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
-              }//in_array($user_id, $arr)  
+          $friend_id_list = (pg_fetch_object($result))->friend_id_list;
+          $arr = explode(",",$friend_id_list);
+          if (in_array($friend_id, $arr)) 
+          {
+              if(sizeof($arr)>1){
+                unset($arr[array_search($friend_id,$arr)]);
+                $string = implode(",",$arr);
+                //update
+                $sql = "UPDATE public.friends SET friend_id_list = '$string' WHERE user_id = '$user_id'";
+                $dbconnection->execute($sql);
+              } //sizeof($arr2)>1
+              else {
+                $sql = "DELETE FROM public.friends WHERE user_id = '$user_id'"; 
+                $dbconnection->execute($sql);
+              }
+
+              $sql = "SELECT friend_id_list FROM public.friends WHERE user_id = '$friend_id'";
+              $result2 = $dbconnection->select($sql);
+              if($result2!==null){
+                  if(pg_num_rows($result2)>0){
+                      $friend_id_list = (pg_fetch_object($result))->friend_id_list;
+                      $arr2 = explode(",",$friend_id_list);
+                      if (in_array($user_id, $arr2)){
+                          if(sizeof($arr2)>1){
+                            unset($arr2[array_search($user_id,$arr)]);
+                            $string2 = implode(",",$arr);
+                            //update
+                            $sql = "UPDATE public.friends SET friend_id_list = '$string2' WHERE user_id = '$user_id'";
+                            $dbconnection->execute($sql);  
+                          }//sizeof($arr2)>1
+                          else{
+                            $sql = "DELETE FROM public.friends WHERE user_id = '$friend_id'"; 
+                            $dbconnection->execute($sql);
+                          }
+                      }//in_array($user_id, $arr2)
+                       else{
+                          $res = new Result(Constant::INVALID_FRIEND, 'Friend is not exist.');
+                       }
+                  }//pg_num_rows($result2)>0
+                  else{
+                     $res = new Result(Constant::INVALID_FRIEND, 'Friend is not exist.');
+                  }
+              }//$result2!==null
               else{
-                  $res = new Result(Constant::INVALID_FRIEND, 'Friend is not exist.');
-               }  
+                  $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+              }
+              $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
+          }//in_array($user_id, $arr)  
+          else{
+              $res = new Result(Constant::INVALID_FRIEND, 'Friend is not exist.');
+           }  
       }//pg_num_rows($result)>0
       else{
         $res = new Result(Constant::INVALID_USER, 'User is not exist');
