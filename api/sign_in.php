cc2c20ac62bc54f48e47a8bce75f46dd2c43e66d
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-05-10 08:24:33 +0700

    Update sign_in.php

--- api/sign_in.php
+++ api/sign_in.php
@@ -20,47 +20,45 @@ if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["devi
     $result = $dbconnection->select($sql);
 
     if ($result !== null) {
-        if (pg_num_rows($result) > 0) {
-            //user exist, check password
-            $dbpassword = null;
-            $user = null;
-            while ($data = pg_fetch_object($result)) {
-                $dbpassword = $data->pass_word;
-                $user = $data;
-                break;
-            }
-            if ($dbpassword !== null) {
-                if (strcasecmp($dbpassword, $password) == 0) {
-                    include "../lib/function.php";
-                    $guid = GUID();
-                    $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
-                    unset($user->pass_word);
-                    $res->sesson_key = $guid;
-                    $res->data = $user;
-                    $user_id = $user->user_id;
-                    //Set online status that was login successfully
-                    setOnline($dbconnection,$user_id,$device_id);
-
-                    //Token: user_id, timestart, timelife
-                    date_default_timezone_set("Asia/Ho_Chi_Minh");
-                    $time = time();
-                    $token_raw = (json_encode(new Token($user_id,$device_id,$time,604800)));
-                    //Encrypt token
-                    $token = encryptData($token_raw,KEY_ENCRYPT);
-                    //Thêm token
-                    $res->token = $token;
-                             
-                    
-                } else {
-                    $res = new Result(Constant::INVALID_PASSWORD, 'Password is not matching.');
-                }
-            } else {
-                $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-            }
-        } else {
-            $res = new Result(Constant::INVALID_USER, 'User is not exist');
+      if (pg_num_rows($result) > 0) {
+        //user exist, check password
+        $dbpassword = null;
+        $user = null;
+        while ($data = pg_fetch_object($result)) {
+            $dbpassword = $data->pass_word;
+            $user = $data;
+            break;
         }
-        $dbconnection->closeResult($result);
+        if ($dbpassword !== null) {
+//           if (strcasecmp($dbpassword, $password) == 0) {
+//             include "../lib/function.php";
+//             $guid = GUID();
+//             $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
+//             unset($user->pass_word);
+//             $res->sesson_key = $guid;
+//             $res->data = $user;
+//             $user_id = $user->user_id;
+//             //Set online status that was login successfully
+//             setOnline($dbconnection,$user_id,$device_id);
+
+//             //Token: user_id, timestart, timelife
+//             date_default_timezone_set("Asia/Ho_Chi_Minh");
+//             $time = time();
+//             $token_raw = (json_encode(new Token($user_id,$device_id,$time,604800)));
+//             //Encrypt token
+//             $token = encryptData($token_raw,KEY_ENCRYPT);
+//             //Thêm token
+//             $res->token = $token;                  
+//           } else {
+//               $res = new Result(Constant::INVALID_PASSWORD, 'Password is not matching.');
+//           }
+        } else {
+            $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+        }
+      } else {
+          $res = new Result(Constant::INVALID_USER, 'User is not exist');
+      }
+      $dbconnection->closeResult($result);
     } else {
         $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
     }
